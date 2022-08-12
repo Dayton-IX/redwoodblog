cc3c063f8f44f7cc650bc7d4e9e5d4aed63cb944
Max Dayton <daytonmaximus@gmail.com>
2022-08-11 20:21:17 -0600

    working on forms

---
+++ web/src/pages/ContactPage/ContactPage.tsx
@@ -0,0 +1,25 @@
+import { Form, Submit, SubmitHandler, TextField } from '@redwoodjs/forms'
+import { MetaTags } from '@redwoodjs/web'
+
+interface FormValues {
+  input: string
+}
+
+const ContactPage = () => {
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    console.log(data)
+  }
+
+  return (
+    <>
+      <MetaTags title="Contact" description="Contact page" />
+
+      <Form onSubmit={onSubmit}>
+        <TextField name="input" />
+        <Submit>Save</Submit>
+      </Form>
+    </>
+  )
+}
+
+export default ContactPage
